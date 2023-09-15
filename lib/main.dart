079b350618403c2ae21fc1fa12e2525efda29f33
Tiga! <maxiinne@proton.me>
2023-09-15 18:09:25 -0300

    create base listing page

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:coodesh/modules/favorites/favorite_page.dart';
+import 'package:coodesh/modules/history/history_page.dart';
+import 'package:coodesh/modules/list/list_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  AppState createState() => AppState();
+}
+
+class AppState extends State<App> with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 3, // Number of tabs
+        child: Scaffold(
+          appBar: AppBar(
+            title: TabBar(
+              controller: _tabController,
+              tabs: const [
+                Tab(text: 'List'),
+                Tab(text: 'History'),
+                Tab(text: 'Favorites'),
+              ],
+            ),
+          ),
+          body: TabBarView(
+            controller: _tabController,
+            children: const [
+              ListPage(),
+              HistoryPage(),
+              FavoritePage()
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/modules/list/list_page.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+class ListPage extends StatefulWidget {
+  const ListPage({super.key});
+
+  @override
+  ListPageState createState() => ListPageState();
+}
+
+class ListPageState extends State<ListPage> {
+  final List<int> _items = List.generate(20, (index) => index); // Initial list of items.
+
+  final ScrollController _scrollController = ScrollController();
+  bool _isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Listen to scroll events and load more data when reaching the end.
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        _loadMoreItems();
+      }
+    });
+  }
+
+  // Simulate loading more items.
+  Future<void> _loadMoreItems() async {
+    if (!_isLoading) {
+      setState(() {
+        _isLoading = true;
+      });
+
+      // Simulate loading delay (you can replace this with your data fetching logic).
+      await Future.delayed(const Duration(seconds: 2));
+
+      setState(() {
+        _items.addAll(List.generate(20, (index) => index + _items.length));
+        _isLoading = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GridView.builder(
+        controller: _scrollController,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+        ),
+        itemCount: _items.length + 1, // Add 1 for loading indicator.
+        itemBuilder: (context, index) {
+          if (index < _items.length) {
+            return GridTile(
+              child: Center(
+                child: Text('Item ${_items[index]}'),
+              ),
+            );
+          } else if (_isLoading) {
+            // Loading indicator when loading more data.
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else {
+            // Reached the end of the list.
+            return const Center(
+              child: Text('End of List'),
+            );
+          }
+        },
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+}
